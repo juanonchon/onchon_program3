d6fca95442d091d6f6d91515bc5ebddb3fd16a52
juanonchon <a.d.f.m36dash@gmail.com>
2024-03-07 21:54:53 -0400

    lo del jueves 7-03-24

---
+++ rotar numeros/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Rotar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            rotar();
+            
+        }
+
+        static void rotar()
+        {
+            string numero = Console.ReadLine();
+            char ultimo = numero[numero.Length - 1];
+            string resto = numero.Substring(0, numero.Length - 1);
+            string resultado = ultimo + resto;
+            Console.WriteLine(resultado);
+        }
+    }
+}
